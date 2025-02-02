6c82ca5ff4ed266d14a18b5aca23c6e9abdfefb5
Syed Zaheed <139610883+szaid47@users.noreply.github.com>
2025-02-02 23:57:21 +0530

    02/02/25

---
+++ numberOfgoodPairs_32.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        res=0
+        for n, c in count.items():
+            res += c*(c-1)//2
+
+        return res
